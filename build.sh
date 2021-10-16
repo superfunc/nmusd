a15c4a946c42669d426da1c6287ee98a6013a0b6
superfunc <superfunc@users.noreply.github.com>
2021-10-15 22:01:05 -0700

    Remove hardcoded paths, accidentally add templating

--- build.sh
+++ build.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/bash
 
+set -o xtrace 
+
+
 if ! [ -d "deps/usd" ]; then
     echo "Performing initial build of USD"
     git clone https://github.com/pixaranimationstudios/usd ./deps/usd/
     pushd deps/usd
     mkdir -p build/inst
     pushd build
+    build_dir=$(pwd)
     python3 ../build_scripts/build_usd.py --no-python --no-tools \
                                          --no-docs --no-tutorials --no-imaging \
-                                         --no-tests --no-examples --verbose (pwd)/inst
+                                         --no-tests --no-examples --verbose "${build_dir}/inst"
  
     popd
     popd
@@ -16,8 +20,16 @@ else
     echo "USD already built, building nim bindings"
 fi
 
-nim cpp --cincludes:/home/superfunc/code/pkgs/usd/build/inst/include/ \
-        --clibdir:/home/superfunc/code/pkgs/usd/build/inst/lib/ \
+curr=$(pwd)
+if ! [ -f "./samples.nim" ]; then
+    echo "Generating nim binding file"
+    sed "s/<###>/$curr\//g" samples.nim.template >> samples.nim
+fi
+
+export _INCLUDES="${curr}/deps/usd/build/inst/include/"
+export _LIBDIR="${curr}/deps/usd/build/inst/lib/"
+nim cpp --cincludes:"$_INCLUDES" \
+        --clibdir:"$_LIBDIR" \
         --passL:-ltf \
         -o:samples \
         samples
