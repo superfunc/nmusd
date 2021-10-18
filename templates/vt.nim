e1215908c09ba504f711ed7afb80ace67ab2b76c
superfunc <superfunc@users.noreply.github.com>
2021-10-17 19:46:39 -0700

    Tinkering with VtValue
    Trying to figure out my way around wrapping generics.
    This may be getting funky because of nim defects, but
    it could be because of the weird type erasure stuff Vt does.

--- templates/vt.nim
+++ templates/vt.nim
@@ -5,6 +5,8 @@
 using namespace pxr;
 """.}
 
+import options
+
 const
     vtValueHeader = "<###>usd/build/inst/include/pxr/base/vt/value.h"
 
@@ -12,7 +14,59 @@ type
     VtValue {.header: vtValueHeader,
               importcpp: "VtValue".} = object
 
-proc CreateVtValue(): VtValue {. header: vtValueHeader, 
+proc createVtValue(): VtValue {. header: vtValueHeader, 
                                  importcpp: "VtValue()" .}
 
+proc createVtValue(k: cint): VtValue {. header: vtValueHeader, 
+                                        importcpp: "VtValue(@)" .}
+
+proc isEmpty(v: VtValue): bool {. header: vtValueHeader,
+                                  importcpp: "#.IsEmpty()" .}
+
+proc priv_uncheckedGet(v: VtValue): cint {. header: vtValueHeader,
+                                            importcpp: "#.UncheckedGet<int>()" .}
+
+proc priv_swap(v: VtValue, k: cint): void {. header: vtValueHeader,
+                                             importcpp: "#.Swap<int>(@)" .}
+
+proc priv_isHolding(v: VtValue): bool {. header: vtValueHeader,
+                                         importcpp: "#.IsHolding<int>()" .}
+
+proc priv_take(k: cint): VtValue {. header: vtValueHeader,
+                                    importcpp: "VtValue::Take(@)" .}
+
+proc get(v: VtValue): Option[cint] =
+    if v.isEmpty():
+        none[cint]()
+    else:
+        some(v.priv_uncheckedGet())
+
+proc set(v: VtValue, k: cint): VtValue =
+    priv_take(k)
+
 echo "Vt module"
+echo "--------------------------------------------------"
+
+let x : cint = 5
+var v = createVtValue(61)
+echo "Is empty?", v.isEmpty(), " ", repr(v.addr)
+echo "Is holding?", priv_isHolding(v)
+echo "Get value: ", v.get()
+v = v.set(65)
+echo "Is empty?", v.isEmpty(), " ", repr(v.addr)
+echo "Is holding?", priv_isHolding(v)
+echo "Get value: ", v.get()
+
+echo ""
+
+var v2 = createVtValue()
+echo "Is empty?", v2.isEmpty(), " ", repr(v2.addr)
+echo "Is holding?", priv_isHolding(v2)
+v2 = v2.set(65)
+echo "Is empty?", v2.isEmpty(), " ", repr(v2.addr)
+echo "Is holding?", priv_isHolding(v2)
+echo "Get value: ", v2.get()
+
+echo "--------------------------------------------------"
+echo "End Vt module"
+echo "--------------------------------------------------"
