76abecc4d79923b0f7cdcdf1460ad2a2b36ec3e5
superfunc <superfunc@users.noreply.github.com>
2021-10-18 01:40:32 -0700

    Noodling with Sdf continues
    Figuring out many strategic things before going 100mph building
    this out. Namely, naming conventions, proper FFI strats, and
    how to handle std::vec<> <-> seq[] conversions.
    
    Once these are squared away 1) turn it into a proper nimble build
    and 2) start proping up testament.

--- templates/sdf.nim
+++ templates/sdf.nim
@@ -2,12 +2,17 @@
 
 {.emit:"""
 #include <pxr/pxr.h>
+#include <pxr/usd/sdf/declareSpec.h>
+#include <pxr/usd/sdf/declareHandles.h>
+#include <pxr/usd/sdf/spec.h>
+#include <pxr/usd/sdf/primSpec.h>
 #include <pxr/usd/sdf/layer.h>
+
 #include <string>
 #include <cstdio>
+#include <vector>
 using namespace pxr;
 
-NIM_EXTERNC
 void priv_dumpLayerContentsC(const char* path) 
 {
     pxr::SdfLayerRefPtr layer = pxr::SdfLayer::FindOrOpen(path);
@@ -15,6 +20,23 @@ void priv_dumpLayerContentsC(const char* path)
     std::string result;
     layer2->ExportToString(&result); 
     printf("%s\n", result.c_str());
+}
+
+std::vector<pxr::SdfPrimSpecHandle> priv_getRootPrimsC(pxr::SdfLayerRefPtr layer) 
+{
+    std::vector<pxr::SdfPrimSpecHandle> rootPrims;
+    for(const pxr::SdfPrimSpecHandle prim : layer->GetRootPrims()) {
+        rootPrims.push_back(prim);
+    }
+
+    return rootPrims;
+}
+
+pxr::SdfPrimSpecHandle priv_pop(std::vector<pxr::SdfPrimSpecHandle>& v) 
+{
+    pxr::SdfPrimSpecHandle item = *v.rbegin(); 
+    v.pop_back();
+    return item;
 }
 
 """.}
@@ -25,6 +47,8 @@ const
     sdfPrimHeader = "<###>usd/build/inst/include/pxr/usd/sdf/primSpec.h"
 
 type
+    StdVec[T] {. header: "<vector>", importcpp: "std::vector" .} = object
+
     SdfLayer {.header: sdfLayerHeader,
                importcpp: "SdfLayer".} = object {. inheritable .}
 
@@ -34,6 +58,14 @@ type
     SdfPrimSpec {. header: sdfPrimHeader,
                    importcpp: "SdfPrimSpec".} = object
 
+    SdfPrimSpecHandle {. header: sdfHandlesHeader,
+                         importcpp: "pxr::SdfPrimSpecHandle".} = object
+
+proc empty(v: StdVec[SdfPrimSpecHandle]): bool {. importcpp: "#.empty()", header: "<vector>" .}
+
+proc next(v: StdVec[SdfPrimSpecHandle]): SdfPrimSpecHandle  {. importcpp: "priv_pop(@)" .}
+
+proc getRootPrims(l: SdfLayerRefPtr): StdVec[SdfPrimSpecHandle] {. importcpp: "priv_getRootPrimsC(@)" .}
 
 proc dumpLayer(s: cstring): void {. importcpp: "priv_dumpLayerContentsC(@)",
                                     nodecl}
@@ -44,13 +76,22 @@ proc openSdfLayer(s: cstring): SdfLayerRefPtr {. importcpp: "SdfLayer::FindOrOpe
 proc getRealPath(l: SdfLayerRefPtr): cstring {. importcpp: "#->GetRealPath().c_str()",
                                                 header: sdfLayerHeader .} 
 
+proc getPsuedoroot(l: SdfLayerRefPtr): SdfPrimSpecHandle {. importcpp: "#->GetPseudoRoot()",
+                                                            header: sdfPrimHeader .} 
 
-#proc getPsuedoroot(l: SdfLayerRefPtr): SdfPrimSpec {. importcpp: "#->GetPseudoRoot()",
-#                                                      header: sdfPrimHeader .} 
-
+proc getName(p: SdfPrimSpecHandle): cstring {. importcpp: "#->GetName().c_str()" 
+                                               header: sdfPrimHeader .}
 
 when isMainModule:
     var 
         l = openSdfLayer("./data/layer.sdf")
         path = $l.getRealPath()
-    echo "Opened layer at ", path
+        proot = l.getPsuedoroot()
+        name = $proot.getName()
+        rootPrims = l.getRootPrims()
+
+    while not rootPrims.empty():
+        var r = rootPrims.next()
+        echo "root prim name: ", r.getName()
+
+    echo "Opened layer at ", path, " with pseudoroot: ", name
