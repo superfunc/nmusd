274dca7fffd73b5a0d2954e6a55623c48dad9ee4
superfunc <superfunc@users.noreply.github.com>
2021-10-18 00:40:02 -0700

    So far, refptrs seem to work?
    Open questions:
    - How to handle cstring and general ctype things as they cross FFI
    boundary? Should they be converted into proper nim types? What are
    the lifetime implications of this.

--- templates/sdf.nim
+++ templates/sdf.nim
@@ -21,16 +21,36 @@ void priv_dumpLayerContentsC(const char* path)
 
 const
     sdfLayerHeader = "<###>usd/build/inst/include/pxr/usd/sdf/layer.h"
-
+    sdfHandlesHeader = "<###>usd/build/inst/include/pxr/usd/sdf/declareHandles.h"
+    sdfPrimHeader = "<###>usd/build/inst/include/pxr/usd/sdf/primSpec.h"
 
 type
     SdfLayer {.header: sdfLayerHeader,
                importcpp: "SdfLayer".} = object {. inheritable .}
-    
+
+    SdfLayerRefPtr {. header: sdfHandlesHeader,
+                      importcpp: "SdfLayerRefPtr".} = object
+
+    SdfPrimSpec {. header: sdfPrimHeader,
+                   importcpp: "SdfPrimSpec".} = object
+
 
 proc dumpLayer(s: cstring): void {. importcpp: "priv_dumpLayerContentsC(@)",
                                     nodecl}
 
+proc openSdfLayer(s: cstring): SdfLayerRefPtr {. importcpp: "SdfLayer::FindOrOpen(@)",
+                                                 header: sdfLayerHeader .} 
+
+proc getRealPath(l: SdfLayerRefPtr): cstring {. importcpp: "#->GetRealPath().c_str()",
+                                                header: sdfLayerHeader .} 
+
+
+#proc getPsuedoroot(l: SdfLayerRefPtr): SdfPrimSpec {. importcpp: "#->GetPseudoRoot()",
+#                                                      header: sdfPrimHeader .} 
+
+
 when isMainModule:
-    echo "Sdf module"
-    dumpLayer("./data/layer.sdf")
+    var 
+        l = openSdfLayer("./data/layer.sdf")
+        path = $l.getRealPath()
+    echo "Opened layer at ", path
