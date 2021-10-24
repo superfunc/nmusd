e6d164a6e110685c817ea0a60e9d435300695baa
superfunc <superfunc@users.noreply.github.com>
2021-10-24 16:45:06 -0700

    Current workaround for compiler crash.
    For some reason nim cpp is having trouble with generics
    surrounding VtValue, could be because of its weird type
    erasure things, not sure just a wild guess. To get around
    this, for now, typedesc can be used and hopefully, since
    that information is all static the compiler can work out
    the direct static calls that switch statement is really
    doing.

--- templates/vt.nim
+++ templates/vt.nim
@@ -35,6 +35,33 @@ proc priv_isHolding(v: VtValue): bool {. header: vtValueHeader,
 proc priv_take(k: cint): VtValue {. header: vtValueHeader,
                                     importcpp: "pxr::VtValue::Take(@)" .}
 
+proc has_cint(v: VtValue): bool {. header: vtValueHeader,
+                                   importcpp: "#.IsHolding<int>()" .}
+
+proc has_cbool(v: VtValue): bool {. header: vtValueHeader,
+                                    importcpp: "#.IsHolding<bool>()" .}
+
+proc has_cfloat(v: VtValue): bool {. header: vtValueHeader,
+                                     importcpp: "#.IsHolding<float>()" .}
+
+proc has_cdouble(v: VtValue): bool {. header: vtValueHeader,
+                                      importcpp: "#.IsHolding<double>()" .}
+
+
+proc has(v: VtValue, t: typedesc): bool =
+    if t is cint:
+        has_cint(v)
+    elif t is bool:
+        has_cbool(v)
+    elif t is float32:
+        has_cfloat(v)
+    elif t is float64:
+        has_cdouble(v)
+    else:
+        false
+
+
+
 proc get(v: VtValue): Option[cint] =
     if v.isEmpty():
         none[cint]()
@@ -45,29 +72,31 @@ proc get(v: VtValue): Option[cint] =
 proc set(v: var VtValue, k: cint): VtValue =
     v = priv_take(k)
 
-# echo "Vt module"
-# echo "--------------------------------------------------"
-# 
-# let x : cint = 5
-# var v = createVtValue(61)
-# echo "Is empty?", v.isEmpty(), " ", repr(v.addr)
-# echo "Is holding?", priv_isHolding(v)
-# echo "Get value: ", v.get()
-# #v.set(65)
-# echo "Is empty?", v.isEmpty(), " ", repr(v.addr)
-# echo "Is holding?", priv_isHolding(v)
-# echo "Get value: ", v.get()
-# 
-# echo ""
-# 
-# var v2 = createVtValue()
-# echo "Is empty?", v2.isEmpty(), " ", repr(v2.addr)
-# echo "Is holding?", priv_isHolding(v2)
-# #v2 = v2.set(65)
-# echo "Is empty?", v2.isEmpty(), " ", repr(v2.addr)
-# echo "Is holding?", priv_isHolding(v2)
-# echo "Get value: ", v2.get()
-# 
-# echo "--------------------------------------------------"
-# echo "End Vt module"
-# echo "--------------------------------------------------"
+echo "Vt module"
+echo "--------------------------------------------------"
+
+let x : cint = 5
+var v = createVtValue(61)
+echo "Is empty?", v.isEmpty(), " ", repr(v.addr)
+echo "Is holding?", priv_isHolding(v)
+echo "Is holding <typed int>?", has(v, cint)
+echo "Is holding <typed bool>?", has(v, bool)
+echo "Get value: ", v.get()
+#v.set(65)
+echo "Is empty?", v.isEmpty(), " ", repr(v.addr)
+echo "Is holding?", priv_isHolding(v)
+echo "Get value: ", v.get()
+
+echo ""
+
+var v2 = createVtValue()
+echo "Is empty?", v2.isEmpty(), " ", repr(v2.addr)
+echo "Is holding?", priv_isHolding(v2)
+#v2 = v2.set(65)
+echo "Is empty?", v2.isEmpty(), " ", repr(v2.addr)
+echo "Is holding?", priv_isHolding(v2)
+echo "Get value: ", v2.get()
+
+echo "--------------------------------------------------"
+echo "End Vt module"
+echo "--------------------------------------------------"
